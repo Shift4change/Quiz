1380dbc53c67bb96a42457b26aaa2fae49f0b95f
Paul Fodrovics <pefodrovics@gmail.com>
2020-02-16 18:46:57 -0500

    quiz redone

--- script.js
+++ script.js
@@ -1,141 +1,118 @@
-let startButton = document.getElementById("start-btn")
-let nextButton = document.getElementById("next-btn")
-// let restartButton=document.getElementById("restart-btn")
 
-let questionContainerElement = document.getElementById("question-container")
-let questionElement = document.getElementById("question")
-let answerButtonsElement = document.getElementById("answer-buttons")
+var startContainer = document.getElementById("bun-venit");
+let quizContainer = document.getElementById("contain-quiz")
+let submitButton = document.getElementById("submit");
+let timpTp = document.getElementById("timp");
+let fragen = document.getElementById("intrebari");
+let aRaspuns = document.getElementById("choice-A");
+let bRaspuns = document.getElementById("choice-B");
+let cRaspuns = document.getElementById("choice-C");
+let dRaspuns = document.getElementById("choice-D");
+let sfarsitContainer = document.getElementById("sfarsit");
+var catTimp = 60;
+//
+//
+//
+// 
 
-let answerButtons = document.getElementById("answer-buttons")
-
-startButton.addEventListener("click", startGame)
-// restartButton.addEventListener("click",restartGame)
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
-function startGame() {
-    console.log("started")
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide")
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        let button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    });
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-            (answerButtonsElement.firstChild)
-    }
-}
+var questions = [{
+    q: "What is the Smallest country in South America?",
+    a: "a",
+    c1: "Suriname",
+    c2: "Uruguay",
+    c3: "Paraguay",
+    c4: "Ecuador"
+}, {
+    q: " What is the biggest muscle in the arm?",
+    a: "c",
+    c1: "Rhomboids",
+    c2: "Biceps",
+    c3: "Trapezius",
+    c4: "Latissimus Dorsi"
+}, {
+    q: "What is Second most popular sport in the world?",
+    a: "d",
+    c1: "soccer",
+    c2: "Baseball",
+    c3: "Basketball",
+    c4: "Cricket"
+}];
 
 
-function selectAnswer(e) {
-    let selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    restartButton.classList.add("hide")
-    // if (shuffledQuestions.length > currentQuestionIndex +1) {  
-    // nextButton.classList.remove("hide")
-    // } else {
-    //     restartButton.classList.remove("hide")
-    // }
-}
-    function setStatusClass(element, correct) {
-        clearStatusClass(element)
-        if (correct) {
-            element.classList.add("correct")
-        } else {
-            element.classList.add("wrong")
-        }
-    }
-    function clearStatusClass(element) {
-        element.classList.remove("correct")
-        element.classList.remove("wrong")
-    }
 
-    let questions = [
-        {
-            question: "what is the Smallest country in South America?",
-            answers: [
-                {
-                    text: "Paraguay", correct: false
-                }, {
-                    text: "Suriname", correct: true
-                }, {
-                    text: "Uruguay", correct: false
-                }, {
-                    text: "Ecuador", correct: false
-                }
 
-            ],
+//
+//
+//
 
-            question: " what's the biggest muscle in the arm?",
-            answers: [
-                {
-                    text: "Brachialis", correct: false
-                }, {
-                    text: "Triceps", correct: true
-                }, {
-                    text: "Biceps", correct: false
-                }, {
-                    text: "Coracobrachialis", correct: false
-                }
+submitButton.addEventListener("click", function incarcaIntr() {
 
-            ],
+    startContainer.style.visibility = "hidden";
+    quizContainer.style.visibility = "visible";
 
-            question: "what is Longest muscle of the back?",
-            answers: [
-                {
-                    text: "Trapezius", correct: false
-                }, {
-                    text: "Latissimus Dorsi", correct: false
-                }, {
-                    text: "Rhomboids", correct: false
-                }, {
-                    text: "Longissmus", correct: true
-                }
 
-            ],
 
-            question: "what is Second most popular sport in the world?",
-            answers: [
-                {
-                    text: "Baseball", correct: false
-                }, {
-                    text: "Basketball", correct: false
-                }, {
-                    text: "Cricket", correct: true
-                }, {
-                    text: "Volleyball", correct: false
-                }
+   
+    var timeInterval = setInterval(function () {
+        timpTp.textContent = catTimp + " seconds remaining";
+        catTimp--;
 
-            ]
+
+        if (catTimp === 0) {
+            timpTp.textContent = "";
+
+            clearInterval(timeInterval);
+            endGame();
         }
 
-    ]
+    }, 1000);
+
+    puneIntrebari();
+
+
+
+
+    function puneIntrebari() {
+        var i = 0;
+        var questIntr = questions[i];
+        var raspunsCorrect = questIntr.a;
+        console.log(raspunsCorrect);
+        fragen.textContent = questIntr.q;
+        aRaspuns.textContent = questIntr.c1;
+        bRaspuns.textContent = questIntr.c2;
+        cRaspuns.textContent = questIntr.c3;
+        dRaspuns.textContent = questIntr.c4;
+
+        aRaspuns.addEventListener('click', function () {
+            
+            console.log(questIntr.c1);
+        });
+        bRaspuns.addEventListener('click', function () {
+            
+
+        });
+        cRaspuns.addEventListener('click', function () {
+          
+
+        });
+        dRaspuns.addEventListener('click', function () {
+            
+            console.log(questIntr.c4);
+        });
+
+
+        if (raspunsCorrect === true) {
+            console.log(raspunsCorrect);
+        }
+    }
+
+
+  
+
+    function endGame() {
+        sfarsitContainer.style.visibility = "visible";
+        quizContainer.style.visibility = "hidden";
+
+    }
+    
+});
