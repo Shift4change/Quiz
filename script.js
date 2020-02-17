e3e0874d7fb6b54dd77e8b133a1d3aa84ca0bfbf
Paul Fodrovics <pefodrovics@gmail.com>
2020-02-16 22:01:59 -0500

    next question display

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let bRaspuns = document.getElementById("choice-B");
 let cRaspuns = document.getElementById("choice-C");
 let dRaspuns = document.getElementById("choice-D");
 let sfarsitContainer = document.getElementById("sfarsit");
+let sfarsitH =document.getElementById("sfarsit-happy");
 var catTimp = 60;
 //
 //
@@ -52,69 +53,114 @@ submitButton.addEventListener("click", function incarcaIntr() {
 
 
 
-   
-    let timeInterval = setInterval(function () {
-        timpTp.textContent = catTimp + " seconds remaining";
-        catTimp--;
-
-
-        if (catTimp === 0) {
-            timpTp.textContent = "";
-
-            clearInterval(timeInterval);
-            endGame();
-        }
-
-    }, 1000);
-
-    puneIntrebari();
-
-
-
-
-    function puneIntrebari() {
-        let i = 0;
-        let questIntr = questions[i];
-        let raspunsCorrect = questIntr.a;
-        console.log(raspunsCorrect);
-        fragen.textContent = questIntr.q;
-        aRaspuns.textContent = questIntr.c1;
-        bRaspuns.textContent = questIntr.c2;
-        cRaspuns.textContent = questIntr.c3;
-        dRaspuns.textContent = questIntr.c4;
-
-        aRaspuns.addEventListener('click', function () {
-            
-            console.log(questIntr.c1);
-        });
-        bRaspuns.addEventListener('click', function () {
-            
-
-        });
-        cRaspuns.addEventListener('click', function () {
-          console.log(raspunsCorrect);
-
-        });
-        dRaspuns.addEventListener('click', function () {
-            
-            console.log(questIntr.c4);
-        });
-
-
-        if (raspunsCorrect === true) {
-            let raspunsCorrect = a
-            console.log(raspunsCorrect);
-            console.log("waht is the value of raspuns Correct"+raspunsCorrect)
-        }
-    }
-
-
-  
-
-    function endGame() {
-        sfarsitContainer.style.visibility = "visible";
-        quizContainer.style.visibility = "hidden";
-
-    }
-    
 });
+let timeInterval = setInterval(function () {
+    timpTp.textContent = catTimp + " seconds remaining";
+    catTimp--;
+
+
+    if (catTimp === 0) {
+        timpTp.textContent = "";
+
+        clearInterval(timeInterval);
+        endGame();
+    }
+
+}, 1000);
+
+puneIntrebari();
+
+
+
+
+function puneIntrebari() {
+    let i = 0;
+    let questIntr = questions[i];
+    let raspunsCorrect = questIntr.a;
+    console.log(raspunsCorrect);
+    fragen.textContent = questIntr.q;
+    aRaspuns.textContent = questIntr.c1;
+    console.log(aRaspuns)
+    bRaspuns.textContent = questIntr.c2;
+    cRaspuns.textContent = questIntr.c3;
+    dRaspuns.textContent = questIntr.c4;
+}
+
+aRaspuns.addEventListener('click', answerButton);
+
+    function answerButton(){
+    let i = 1;
+    let questIntr = questions[i];
+    let raspunsCorrect = questIntr.a;
+    console.log(raspunsCorrect);
+    fragen.textContent = questIntr.q;
+    aRaspuns.textContent = questIntr.c1;
+    console.log(aRaspuns);
+    bRaspuns.textContent = questIntr.c2;
+    cRaspuns.textContent = questIntr.c3;
+    dRaspuns.textContent = questIntr.c4;
+    console.log(answerButton);
+    console.log(questIntr.c1);
+    }
+
+    
+cRaspuns.addEventListener('click', answerButton1);
+
+function answerButton1(){
+let i = 2;
+let questIntr = questions[i];
+let raspunsCorrect = questIntr.a;
+console.log(raspunsCorrect);
+fragen.textContent = questIntr.q;
+aRaspuns.textContent = questIntr.c1;
+console.log(aRaspuns);
+bRaspuns.textContent = questIntr.c2;
+cRaspuns.textContent = questIntr.c3;
+dRaspuns.textContent = questIntr.c4;
+console.log(answerButton);
+console.log(questIntr.c1);
+}
+
+dRaspuns.addEventListener('click', answerButton2);
+function answerButton2(){
+    
+        sfarsitH.style.visibility = "visible";
+        quizContainer.style.visibility = "hidden";
+    
+    
+}
+
+function endGame() {
+    sfarsitContainer.style.visibility = "visible";
+    quizContainer.style.visibility = "hidden";
+
+}
+
+
+
+// bRaspuns.addEventListener('click', answerButton) {
+
+
+// };
+// cRaspuns.addEventListener('click', answerButton) {
+//     console.log(raspunsCorrect);
+
+// };
+// dRaspuns.addEventListener('click', answerButton) {
+
+//     console.log(questIntr.c4);
+// };
+
+
+// if (raspunsCorrect === true) {
+
+//     console.log(raspunsCorrect);
+//     console.log("waht is the value of raspuns Correct"+raspunsCorrect)
+// }
+
+
+
+
+
+
+
